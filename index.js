828c9d7c530bd3d52f848cbca2d2c8e08d97167e
lovelyboss <73265891+lovelyboss@users.noreply.github.com>
2021-12-24 18:37:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,24 +29,9 @@ const questionTools = [
     message:"Select tools:",
     choices:
       [
-        "[1]  Bot Like Timeline",
-        "[2]  Follow Followers Target by People",
-        "[3]  Follow Followers Target by Media",
-        "[4]  Follow Followers Target by Hastag",
-        "[5]  Follow Followers Target by Location",
-        "[6]  Follow Followers Target by People - with DM",
-        "[7]  Follow Followers Target by People - No Like",
-	      "[8]  Follow Followers Target by People - No Comment & Like",
-        "[9]  Repost Media Target by People",
-        "[10] Repost Media Target by Hashtag",
-        "[11] Repost Media Target by Link",
-        "[12] Comment & Like Followers Target by People",
-        "[13] Comment & Like Followers Target by Hashtag",
-        "[14] Bom Like Target's Post",
-        "[15] Bom Comment Target's Post",
-        "[16] Unfollow Not Followback",
-        "[17] Unfollow All Following",
-        "[18] Delete All Media",
+        
+        "[13] Comment & Like Followers Target by Hashtag"
+       
         ""
       ] 
   }
@@ -58,60 +43,11 @@ const main = async () => {
     var toolChoise = await inquirer.prompt(questionTools);
     toolChoise = toolChoise.Tools;
     switch(toolChoise){
-      case "[1]  Bot Like Timeline":
-        await require("./src/botLikeTimeline.js");
-        break;
-      case "[2]  Follow Followers Target by People":
-        await require("./src/FFTPeople.js");
-        break;
-      case "[3]  Follow Followers Target by Media":
-        await require("./src/FFTMedia.js");
-        break;
-      case "[4]  Follow Followers Target by Hastag":
-        await require("./src/FFTHashtag.js");
-        break;
-      case "[5]  Follow Followers Target by Location":
-        await require("./src/FFTLocation.js");
-        break;
-      case "[6]  Follow Followers Target by People - with DM":
-        await require("./src/FFTPeople-DM.js");
-        break;
-      case "[7]  Follow Followers Target by People - No Like":
-        await require("./src/FFTPeople-noLike.js");
-        break;
-      case "[8]  Follow Followers Target by People - No Comment & Like":
-        await require("./src/FFTPeople-noLikeComment.js");
-        break;
-      case "[9]  Repost Media Target by People":
-        await require("./src/RMTPeople.js");
-        break;
-      case "[10] Repost Media Target by Hashtag":
-        await require("./src/RMTHashtag.js");
-        break;
-      case "[11] Repost Media Target by Link":
-        await require("./src/RMTLink.js");
-        break;
-      case "[12] Comment & Like Followers Target by People":
-        await require("./src/commentLike-byPeople.js");
-        break;
+      
       case "[13] Comment & Like Followers Target by Hashtag":
         await require("./src/commentLike-byHashtag.js");
         break;
-      case "[14] Bom Like Target's Post":
-        await require("./src/bomLike.js");
-        break;
-      case "[15] Bom Comment Target's Post":
-        await require("./src/bomComment.js");
-        break;
-      case "[16] Unfollow Not Followback":
-        await require("./src/unfollowNotFollowBack.js");
-        break;
-      case "[17] Unfollow All Following":
-        await require("./src/unfollowAllFollowing.js");
-        break;
-      case "[18] Delete All Media":
-        await require("./src/deleteAllMedia.js");
-        break;
+     
       default:
         console.log('\nERROR:\n[?] Aw, Snap! \n[!] Something went wrong while displaying this program!\n[!] Please try again!');
     }
@@ -123,12 +59,12 @@ const main = async () => {
 
 console.log(chalk`
   {bold.cyan
-  ╦┌┐┌┌─┐┌┬┐┌─┐┌─┐┬─┐┌─┐┌┬┐
-  ║│││└─┐ │ ├─┤│ ┬├┬┘├─┤│││
-  ╩┘└┘└─┘ ┴ ┴ ┴└─┘┴└─┴ ┴┴ ┴
-  ╔╦╗┌─┐┌─┐┬  ┌─┐    © 2019
-   ║ │ ││ ││  └─┐   SGBTeam 
-   ╩ └─┘└─┘┴─┘└─┘   -------       
+  ╦┌┐┌┌─┐┌┬┐┌─┐
+  ║│││└─┐ │ ├─┤
+  ╩┘└┘└─┘ ┴ ┴ ┴
+  ╔╦╗┌─┐┌─┐┬  ┌─┐    © 2021
+   ║ │ ││ ││  └─┐   BY LOVELYBOY
+   ╩ └─┘└─┘┴─┘└─┘       
 }
       `);
 
